76314689453316d96ee8fdd5523b8292f29f019b
bsindala <bonifacethesaint@gmail.com>
2021-10-13 16:09:50 -0500

    Working with React Dev Tools.

--- TargetSum/src/components/Game.js
+++ TargetSum/src/components/Game.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 import {View, Text, StyleSheet} from 'react-native';
 
+import RandomNumber from './RandomNumber';
+
 class Game extends React.Component {
     static propTypes = {
         randomNumberCount: PropTypes.number.isRequired,
+    };
+    state = {
+        selectedNumbers: [],
     };
     randomNumbers = Array
         .from({ length: this.props.randomNumberCount })
@@ -14,15 +19,29 @@ class Game extends React.Component {
         .slice(0, this.props.randomNumberCount - 2)
         .reduce((acc, curr) => acc + curr, 0);
     
+    isNumberSelected = (numberIndex) => {
+        return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+    };
+    selectNumber = (numberIndex) => {
+        this.setState((prevState) => ({ 
+            selectedNumbers: [...prevState.selectedNumbers, numberIndex], 
+        }));
+    };
 
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.target}>{this.target}</Text>
                 <View style={styles.randomContainer}>
-                    {this.randomNumbers.map((randomNumber, index) =>
-                        <Text style={styles.random} key={index}>{randomNumber}</Text>
-                    )}
+                    {this.randomNumbers.map((randomNumber, index) => (
+                        <RandomNumber 
+                            key={index} 
+                            id={index}
+                            number={randomNumber} 
+                            isDisabled={this.isNumberSelected(index)}
+                            onPress={this.selectNumber}
+                        />
+                    ))}
                 </View>
             </View>
         );
@@ -47,15 +66,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-around',
-    },
-
-    random: {
-        backgroundColor: '#999',
-        width: 100,
-        marginHorizontal: 15,
-        marginVertical: 25,
-        fontSize: 35,
-        textAlign: 'center',
     },
 });
 
